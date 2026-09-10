9103475acc38b4d5d3685f3a5292a311c9397dd4
john <mkushmonov@gmail.com>
2026-09-10 18:54:50 +0900

    feat: meta tags integration on document private page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta charSet="UTF-8" />
+      <meta name="title" content="Nestar Demo" />
+      <meta name="robots" content="index,follow" />
+      <link rel="icon" type="image/png" href="" />
+
+      {/* SEO - Search engine Optimization */}
+      <meta name="keyword" content={"nestar, nestar.uz, devex mern, mern nestjs, mern nestjs fullstack"} />
+      <meta name="description" content={"Buy and sale properties anywhere anytime in South Korea."} />
       <body>
         <Main />
         <NextScript />
